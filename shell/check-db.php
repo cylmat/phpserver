f969c627f42b9cb51844c66f93bd461ef67b53ec
cylmat <cyrilmatte.pro@gmail.com>
2021-03-16 14:25:51 +0100

    Dockerfile

--- shell/check-db.php
+++ shell/check-db.php
@@ -1,67 +1,73 @@
 <?php
 
-Check::pdo('mysql','mysql:host=mysql;dbname=mydb');
+// PDO
 Check::pdo('maria','mysql:host=mariadb;dbname=madb');
-Check::pdo('sqlite','sqlite:/sqlite/sqlite.db3'); 
+Check::pdo('mysql','mysql:host=mysql;dbname=mydb');
+Check::pdo('postg','pgsql:host=postgres;port=5432;dbname=pgdb');
+Check::pdo('sqlit','sqlite:/sqlite/sqlite.db3');
 
-Check::pg('pgsql:host=postgres;port=5432;dbname=pgdb');
+// Php ext for MySql
 Check::odbc("DRIVER={MySQL ODBC 8.0 Unicode Driver};Server=mysql;Database=mydb;Port=3306;String Types=Unicode");
 
+// Key-value
 Check::dba("/tmp/test.db4");
 Check::redis();
 Check::mem();
 
+echo "count:" . (Check::getCount()===8 ? '[OK]' : '[failed]');
 
 /**
  * Check data connections
  */
 class Check
 {
+    static private $count = 0;
+
+    static function getCount()
+    {
+        return self::$count;
+    }
+
     static function pdo(string $type, string $dsn)
     {
-        $table = 'test_tb';
+        $table = 'test';
         try {
             $pdo = new PDO($dsn, $_SERVER['DB_USER'], $_SERVER['DB_PASS']);
             $pdo->exec("SET CHARACTER SET utf8");
-            $pdo->exec("CREATE TABLE IF NOT EXISTS $table (id INT, my TEXT);");
+            $pdo->exec("CREATE TABLE IF NOT EXISTS $table (id INT, my VARCHAR(20))");
             $pdo->exec("TRUNCATE TABLE $table;");
-            $pdo->exec('INSERT INTO '.$table.' (id, my) VALUES (96, " '.$type.':ok ") ON DUPLICATE KEY UPDATE id=id;');
+            $pdo->exec("DELETE FROM $table;");
+            $pdo->exec("INSERT INTO $table (id, my) VALUES (96, 'ok')");
             $r = $pdo->query("SELECT * FROM $table");
-            if (false === $r) throw new PDOException("Query failed");
             if(is_array($res = $r->fetch())) {
-                echo $res['my'] . PHP_EOL;
+                echo $type.':'.$res['my'] . PHP_EOL;
+                self::$count++;
+            } else {
+                var_dump($r->errorInfo()[2]);
+                throw new PDOException("Query $type empty");
             }
         } catch (PDOException $e) {
             echo " $type:".$e->getMessage().PHP_EOL;
         }
     }
 
-    static function pg(string $dsn)
-    {
-        // Postgres
-        try {
-            $pdo = new PDO($dsn, $_SERVER['DB_USER'], $_SERVER['DB_PASS']);
-            $r = $pdo->exec("CREATE TABLE IF NOT EXISTS test_tb (id INT, my TEXT);");
-            if(false !== $r) {
-                echo ' pg-ok'.PHP_EOL;
-            }
-        } catch (PDOException $e) {
-            echo " PG:".$e->getMessage().PHP_EOL;
-        }
-    }
-
     static function odbc(string $dsn)
     {
+        $table = "odbc";
+
         // odbc mysql
         $connection = odbc_connect($dsn, $_SERVER['DB_USER'], $_SERVER['DB_PASS']);
         if(!$connection) {
             echo ' ODBC fail connection '.PHP_EOL;
         }
-        odbc_exec($connection,'INSERT INTO tests_my (id, my) VALUES (21, " odbc-ok ")');
-        $res = odbc_exec($connection, "SELECT * FROM tests_my WHERE id=21");
+        odbc_exec($connection, "CREATE TABLE IF NOT EXISTS $table (id INT, my VARCHAR(20))");
+        odbc_exec($connection, "TRUNCATE TABLE $table;");
+        odbc_exec($connection, "INSERT INTO $table (id, my) VALUES (21, 'ok') ON DUPLICATE KEY UPDATE id=id");
+        $res = odbc_exec($connection, "SELECT * FROM $table WHERE id=21");
         odbc_fetch_row ($res, 0);
         if($r = odbc_result($res, 'my')) {
-            echo $r.PHP_EOL;
+            echo "odbc:$r\n";
+            self::$count++;
         } else {
             echo ' ODBC:fail query '.PHP_EOL;
         }
@@ -78,10 +84,11 @@ class Check
         if (!$dba) {
             echo " dba_open failed \n";
         }
-        dba_replace("key", " dba-ok ".PHP_EOL, $dba);
+        dba_replace("key", "dba:ok".PHP_EOL, $dba);
         if (dba_exists("key", $dba)) {
             echo dba_fetch("key", $dba);
             dba_delete("key", $dba);
+            self::$count++;
         } else {
             echo " DBA:failed \n";
         }
@@ -93,8 +100,9 @@ class Check
         try { 
             $redis = new Redis;
             $redis->connect('redis', 6379);
-            $redis->set("key", " redis-ok ");
+            $redis->set("key", "redis:ok");
             echo $redis->get("key").PHP_EOL;
+            self::$count++;
         } catch(\Exception $e) {
             echo $e->getMessage().PHP_EOL;
         }
@@ -105,12 +113,11 @@ class Check
         try {
             $mc = new Memcached; 
             $mc->addServer("memcached", 11211); 
-            $mc->set("test", " memcached-ok "); 
+            $mc->set("test", "memcached:ok"); 
             echo $mc->get("test").PHP_EOL;
+            self::$count++;
         } catch(\Exception $e) {
             echo $e->getMessage().PHP_EOL;
         }
     }
 }
-
-echo PHP_EOL;
