8843c081fd4c83add8cc75429c1241ff85f0b886
cylmat <cyrilmatte.pro@gmail.com>
2021-03-18 21:17:50 +0100

    in progress

--- public/index.php
+++ public/index.php
@@ -1,2 +1,31 @@
 <?php
 
+if (getenv('ZMQ_SERVER')) {
+    $server = new ZMQSocket(new ZMQContext(), ZMQ::SOCKET_REP);
+    $server->bind("tcp://*:11111");
+    
+    while (true) {
+        $message = $server->recv();
+        $reply = strrev($message);
+        $server->send($reply);
+    }
+    die();
+} 
+
+$ctx = new ZMQContext();
+$req = new ZMQSocket($ctx, ZMQ::SOCKET_REQ);
+$req->connect("tcp://localhost:11111");
+
+$req->send("bonjour");
+$response = $req->recv();
+print("Réponse :  '$response'\n");
+die();
+
+// Nom d'hôte du serveur
+$dsn = "tcp://localhost:11111";
+// Crée un socket
+$socket = new ZMQSocket(new ZMQContext(1), ZMQ::MODE_NOBLOCK, null);
+// Envoi et réception
+$socket->connect($dsn);
+$socket->send("Test", ZMQ::MODE_NOBLOCK);
+$message = $socket->recv(ZMQ::MODE_DONTWAIT);
