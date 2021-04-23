6659fb38830d84de7d23196cbc3b0d747037eb07
cylmat <cyrilmatte.pro@gmail.com>
2021-04-23 21:13:32 +0200

    phpinfo

--- public/index.php
+++ public/index.php
@@ -1,11 +1,13 @@
 <?php
 
 $dbsql = gets('dbsql');
+[$php_css, $php_env] = variables();
 
 echo <<<TEMPLATE
 <!doctype html>
 <html>
 <head>
+    $php_css
     <style type="text/css">
         .g {color:green}
     </style>
@@ -17,11 +19,13 @@ echo <<<TEMPLATE
     <a href="">{$_SERVER['SERVER_SOFTWARE']}</a> [<span class="g">OK</span>]
     <h2>Databases</h2>
     $dbsql
+    <h2>PhpInfo</h2>
+    $php_env
 </body>
 </html>
 TEMPLATE;
 
-function gets(string $type): string{
+function gets(string $type): string {
     $file = __DIR__ . "/../shell/check-$type.php";
     if (file_exists($file)) {
         ob_start();
@@ -29,4 +33,15 @@ function gets(string $type): string{
         return nl2br(ob_get_clean());
     }
     return '';
+}
+
+function variables(): array {
+    ob_start();
+    phpinfo(INFO_VARIABLES | INFO_ENVIRONMENT);
+    $content = ob_get_clean();
+    preg_match("/<style.*style>/", str_replace("\n", "\r", $content), $php_css);
+    preg_match("/<div.*div>/", str_replace("\n", "\r", $content), $php_env);
+    $php_css = str_replace("\r", "\n", $php_css[0]);
+    $php_env = str_replace("\r", "\n", $php_env[0]);
+    return [$php_css, $php_env];
 }
